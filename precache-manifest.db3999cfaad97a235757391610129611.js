7b1cbe010900bba558e569d4bfecf3edfbbfd10a
Code Sandbox <hello@codesandbox.io>
2021-05-18 21:11:41 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.db3999cfaad97a235757391610129611.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "298f5f3da4688fe904dbeb0f1d9bcb80",
+    "url": "https://owensam.github.io/csb-3ivyg/index.html"
+  },
+  {
+    "revision": "3d533bbd0ba6aeefc9a6",
+    "url": "https://owensam.github.io/csb-3ivyg/static/css/main.40845ec0.chunk.css"
+  },
+  {
+    "revision": "0b5c36f87031b2621f21",
+    "url": "https://owensam.github.io/csb-3ivyg/static/js/2.a32197cd.chunk.js"
+  },
+  {
+    "revision": "3d533bbd0ba6aeefc9a6",
+    "url": "https://owensam.github.io/csb-3ivyg/static/js/main.b36dd3a5.chunk.js"
+  },
+  {
+    "revision": "cea1590d3188e5399b08",
+    "url": "https://owensam.github.io/csb-3ivyg/static/js/runtime~main.fbebd7e6.js"
+  }
+]);
